331dc0342c2428f719e2709cd77a88e1c05fd524
deylin-16 <deylin1616@gmail.com>
2026-01-21 10:59:25 -0600

    Actualizar tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'quemado': '#2c1a12',
         'vintage': '#f4e4bc',
       },
       boxShadow: {
