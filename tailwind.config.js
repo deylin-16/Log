dbdf53e2cf200593b5e69755ee89d25fac4c0f43
deylin-16 <deylin1616@gmail.com>
2026-01-21 11:03:35 -0600

    Actualizar tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Esto le dice a Tailwind que busque aquí
   ],
   theme: {
     extend: {
