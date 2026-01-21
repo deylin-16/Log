36f8df8c649a0a1d2e624f828ca1595fbb3150a2
deylin-16 <deylin1616@gmail.com>
2026-01-21 15:17:25 -0600

    Crear src/constants/papers.js

---
+++ src/constants/papers.js
@@ -0,0 +1,10 @@
+export const PAPERS = {
+  classic: "bg-white",
+  notebook: "bg-[#f0f0f0] bg-[linear-gradient(transparent_95%,_#abced4_95%)] bg-[length:100%_30px]",
+  burn: "bg-[#f3e5ab] [box-shadow:inset_0_0_50px_rgba(0,0,0,0.2)]",
+  ancient: "bg-[#c4a484] [box-shadow:inset_0_0_100px_rgba(0,0,0,0.3)]",
+  dark: "bg-[#121212]",
+  carbon: "bg-[#1a1a1a] bg-[url('https://www.transparenttextures.com/patterns/carbon-fibre.png')]",
+  galaxy: "bg-black shadow-[inset_0_0_80px_rgba(79,70,229,0.3)]",
+  love: "bg-[#fff0f3] border-pink-100"
+};
