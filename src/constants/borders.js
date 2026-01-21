8019a24594bd8d41cc6ed79b49633b3bc5325d6e
deylin-16 <deylin1616@gmail.com>
2026-01-21 15:17:57 -0600

    Crear src/constants/borders.js

---
+++ src/constants/borders.js
@@ -0,0 +1,10 @@
+export const BORDERS = {
+  none: "border-none",
+  gold: "border-[15px] border-double border-[#d4af37] ring-4 ring-yellow-900/20 ring-inset",
+  neon: "border-[3px] border-cyan-400 shadow-[0_0_30px_rgba(34,211,238,0.5)]",
+  floral: "border-[20px] border-solid border-[#2d5a27]",
+  retro: "border-[12px] border-double border-red-900",
+  silver: "border-[15px] border-double border-[#C0C0C0]",
+  royal: "border-[20px] border-solid border-[#4b0082] ring-4 ring-yellow-500",
+  glitch: "border-[4px] border-solid border-red-500 shadow-[4px_0_blue,-4px_0_red]"
+};
