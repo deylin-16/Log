c0789427fdea5e38d10fde7db52e9423af2575c2
deylin-16 <deylin1616@gmail.com>
2026-01-21 15:18:17 -0600

    Crear src/constants/fonts.js

---
+++ src/constants/fonts.js
@@ -0,0 +1,7 @@
+export const FONTS = [
+  { id: 1, name: 'Elegante', family: "'Playfair Display', serif" },
+  { id: 2, name: 'Mano', family: "'Caveat', cursive" },
+  { id: 3, name: 'Moderna', family: "'Bebas Neue', sans-serif" },
+  { id: 4, name: 'Escrita', family: "'Dancing Script', cursive" },
+  { id: 5, name: 'Máquina', family: "'Special Elite', cursive" }
+];
