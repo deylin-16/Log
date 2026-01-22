82af308fdcd3621bbd58e5ca8edc85ab739f755c
deylin-16 <deylin1616@gmail.com>
2026-01-22 13:11:17 -0600

    Actualizar shapes.js

--- src/constants/shapes.js
+++ src/constants/shapes.js
@@ -1,11 +1,12 @@
 // constants/shapes.js
+import React from 'react'; // Necesario en la mayoría de entornos React
 
 export const SHAPES = {
   square: () => (
-    <rect x="0" y="0" width="100" height="100" rx="12" ry="12" />
+    <rect x="5" y="5" width="90" height="90" rx="12" ry="12" />
   ),
   rectangle: () => (
-    <rect x="0" y="20" width="100" height="60" rx="8" ry="8" />
+    <rect x="5" y="25" width="90" height="50" rx="8" ry="8" />
   ),
   circle: () => (
     <circle cx="50" cy="50" r="45" />
@@ -23,21 +24,21 @@ export const SHAPES = {
     <polygon points="50,10 90,50 50,90 10,50" />
   ),
   pentagon: () => (
-    <polygon points="50,10 90,35 80,90 20,90 10,35" />
+    <polygon points="50,5 95,38 78,92 22,92 5,38" />
   ),
   hexagon: () => (
-    <polygon points="25,10 75,10 90,50 75,90 25,90 10,50" />
+    <polygon points="25,10 75,10 95,50 75,90 25,90 5,50" />
   ),
   star: () => (
-    <polygon points="50,10 61,35 90,35 65,55 75,90 50,70 25,90 35,55 10,35 39,35" />
+    <polygon points="50,5 63,38 98,38 70,59 78,95 50,75 22,95 30,59 2,38 37,38" />
   ),
   heart: () => (
-    <path d="M50 30 Q 20 0, 0 30 Q 0 60, 25 80 Q 50 100, 75 80 Q 100 60, 100 30 Q 80 0, 50 30 Z" />
+    <path d="M50,85 L44,80 C25,62 10,48 10,32 C10,19 19,10 32,10 C39,10 46,14 50,20 C54,14 61,10 68,10 C81,10 90,19 90,32 C90,48 75,62 56,80 L50,85 Z" />
   ),
-  star4: () => (  // estrella de 4 puntas
-    <polygon points="50,10 60,40 90,50 60,60 50,90 40,60 10,50 40,40" />
+  star4: () => (
+    <polygon points="50,5 62,38 95,50 62,62 50,95 38,62 5,50 38,38" />
   ),
   cloud: () => (
-    <path d="M20,60 Q0,40 20,20 Q40,0 60,20 Q80,0 100,20 Q120,40 100,60 Q80,80 60,60 Q40,80 20,60 Z" />
+    <path d="M25,80 C10,80 5,65 15,55 C10,35 30,20 45,30 C55,10 85,15 85,40 C95,45 95,70 80,80 Z" />
   )
 };
